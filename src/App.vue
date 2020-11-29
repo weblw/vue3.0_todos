e064c526a2fd393359e14942849381b49f3f220a
lwMac <lwmac@lwMacdeMac-mini.local>
2020-11-29 09:18:49 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,195 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section id="app" class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        autocomplete="off"
+        autofocus
+        v-model="input"
+        @keyup.enter="addTodo"
+        >
+    </header>
+    <section class="main" v-show="count">
+      <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone">
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li v-for="todo in filteredTodos" :key="todo" :class="{ editing: todo === editingTodo, completed: todo.completed }">
+          <div class="view">
+            <input class="toggle" type="checkbox" v-model="todo.completed">
+            <label @dblclick="editTodo(todo)">{{ todo.text }}</label>
+            <button class="destroy" @click="remove(todo)"></button>
+          </div>
+          <input
+            v-editing-focus="todo === editingTodo"
+            @keyup.enter="doneEdit(todo)"
+            @blur="doneEdit(todo)"
+            @keyup.esc="cancelTodo(todo)"
+            class="edit"
+            v-model="todo.text"
+            type="text">
+        </li>
+      </ul>
+    </section>
+    <footer class="footer" v-show="count">
+      <span class="todo-count">
+        <strong>{{ remainingCount }}</strong> {{ remainingCount > 1 ? 'items' : 'item' }} left
+      </span>
+      <ul class="filters">
+        <li><a href="#/all">All</a></li>
+        <li><a href="#/active">Active</a></li>
+        <li><a href="#/completed">Completed</a></li>
+      </ul>
+      <button class="clear-completed" @click="removeCompleted" v-show="count > remainingCount">
+        Clear completed
+      </button>
+    </footer>
+  </section>
+  <footer class="info">
+    <p>Double-click to edit a todo</p>
+    <!-- Remove the below line ↓ -->
+    <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+    <!-- Change this out with your name and url ↓ -->
+    <p>Created by <a href="https://www.lagou.com">教瘦</a></p>
+    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+  </footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/index.css'
+import useLocalStorage from './utils/useLocalStorage'
+import { computed, onMounted, onUnmounted, ref, watchEffect } from 'vue'
+
+const storage = useLocalStorage()
+// 添加待办事项
+const useAdd = todos => {
+  const input = ref('')
+  const addTodo = () => {
+    const text = input.value && input.value.trim()
+    if (text.length === 0) return
+    todos.value.unshift({
+      text,
+      completed: false
+    })
+    input.value = ''
+  }
+  return {
+    input,
+    addTodo
+  }
+}
+// 删除待办事项
+const useRemove = todos => {
+  const remove = todo => {
+    const index = todos.value.indexOf(todo)
+    todos.value.splice(index, 1)
+  }
+  const removeCompleted = () => {
+    todos.value = todos.value.filter(todo => !todo.completed)
+  }
+  return {
+    remove,
+    removeCompleted
+  }
+}
+// 编辑待办事项
+const useEdit = (remove) => {
+  let beforeEditText = ''
+  const editingTodo = ref(null)
+  const editTodo = todo => {
+    beforeEditText = todo.text
+    editingTodo.value = todo
+  }
+  const doneEdit = todo => {
+    if (!editingTodo.value) return
+    todo.text = todo.text.trim()
+    todo.text || remove(todo)
+    editingTodo.value = null
+  }
+  const cancelTodo = todo => {
+    editingTodo.value = null
+    todo.text = beforeEditText
+  }
+  return {
+    editingTodo,
+    editTodo,
+    doneEdit,
+    cancelTodo
+  }
+} 
+// 切换待办项完成状态
+const useFilter = todos => {
+  const allDone = computed({
+    get () {
+      return !todos.value.filter(todo => !todo.completed).length
+    },
+    set (value) {
+      todos.value.forEach(todo => {
+        todo.computed = value
+      })
+    }
+  })
+  const filter = {
+    all: list => list,
+    active: list => list.filter(todo => !todo.completed),
+    completed: list => list.filter(todo => todo.completed)
+  }
+  const type = ref('all')
+  const filteredTodos = computed(() => filter[type.value](todos.value))
+  const remainingCount = computed(() => filter.active(todos.value).length)
+  const count = computed(() => todos.value.length)
+  const onHashChange = () => {
+    const hash = window.location.hash.replace('#/', '')
+    if (filter[hash]) {
+      type.value = hash
+    } else {
+      type.value = 'all'
+      window.location.hash = ''
+    }
+  }
+  onMounted(() => {
+    window.addEventListener('hashchange', onHashChange)
+    onHashChange()
+  })
+  onUnmounted(() => {
+    window.removeEventListener('hashchange', onHashChange)
+  })
+  return {
+    allDone,
+    filteredTodos,
+    remainingCount,
+    count
+  }
+}
+// 存储待办事项
+const useStorage = () => {
+  const key = 'TODOKEYS'
+  const todos = ref(storage.getItem(key) || [])
+  watchEffect(() => {
+    storage.setItem(key, todos.value)
+  })
+  return todos
+}
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const todos = useStorage()
+    const { remove, removeCompleted } = useRemove
+    return {
+      todos,
+      remove,
+      removeCompleted,
+      ...useAdd(todos),
+      ...useEdit(remove),
+      ...useFilter(todos)
+    }
+  },
+  directives: {
+    editingFocus: (el, binding) => {
+      binding.value && el.focus()
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
